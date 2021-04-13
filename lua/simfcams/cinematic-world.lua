0d23be5fe167abd235bda22804c17c1df0d08522
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2021-04-13 13:25:11 -0500

    removed dumb from cinematic world cam

--- lua/simfcams/cinematic-world.lua
+++ lua/simfcams/cinematic-world.lua
@@ -1,41 +1,5 @@
-local function DefineSimfForward(vehicle)
-	--get the vehicles list and if it has custom wheels
-	local vehlist = list.Get( "simfphys_vehicles" )[ vehicle:GetSpawn_List() ]
-	local CustomWheels = vehlist.Members.CustomWheels
-	--create a table with all the wheel locations, determines if custom wheel or attachment locations are needed
-	local WheelTable = {}
-	WheelTable.FL = CustomWheels and vehicle:LocalToWorld( vehlist.Members.CustomWheelPosFL ) or vehicle:GetAttachment( vehicle:LookupAttachment( "wheel_fl" ) ).Pos
-	WheelTable.FR = CustomWheels and vehicle:LocalToWorld( vehlist.Members.CustomWheelPosFR ) or vehicle:GetAttachment( vehicle:LookupAttachment( "wheel_fr" ) ).Pos
-	WheelTable.RL = CustomWheels and vehicle:LocalToWorld( vehlist.Members.CustomWheelPosRL ) or vehicle:GetAttachment( vehicle:LookupAttachment( "wheel_rl" ) ).Pos
-	WheelTable.RR = CustomWheels and vehicle:LocalToWorld( vehlist.Members.CustomWheelPosRR ) or vehicle:GetAttachment( vehicle:LookupAttachment( "wheel_rr" ) ).Pos
-
-	--find the local forward direction based on wheel location to angles
-	local pAngL = vehicle:WorldToLocalAngles( ((WheelTable.FL + WheelTable.FR) / 2 - (WheelTable.RL + WheelTable.RR) / 2):Angle() )
-	pAngL.r = 0
-	pAngL.p = 0
-
-	--save the local forward direction
-	vehicle.forward = pAngL
-
-	--find the right direction from the forward direction
-	local yAngL = pAngL - Angle(0,90,0)
-	yAngL:Normalize()
-
-	--save the right local direction
-	vehicle.right = yAngL
-end
-
-local function FindForward(vehicle)
-	if !vehicle.forward then DefineSimfForward(vehicle) end
-	return vehicle:LocalToWorldAngles( vehicle.forward ):Forward()
-end
-local function FindRight(vehicle)
-	if !vehicle.forward then DefineSimfForward(vehicle) end
-	return vehicle:LocalToWorldAngles( vehicle.right ):Forward()
-end
-
 local Cam = {}
-Cam.Name = "Cinematic-World-Only"
+Cam.Name = "Cinematic-World"
 
 Cam.CamChangeDel = CurTime() 
 Cam.CurCam = 1
@@ -45,7 +9,7 @@ Cam.CamFuncs = {}
 Cam.LookPos = Vector(0,0,0)
 
 function Cam:Init()
-	print("Cinematic camera started.")
+	print("Cinematic (world only) camera started.")
 end
 
 --random placement cam
